bec08b33b3addeef5a191d41fa81b60c083d90bc
Marc Schäffner-Gurney <mws8726@gmail.com>
2018-02-23 10:21:48 -0500

    Updates

--- app.js
+++ app.js
@@ -13705,127 +13705,163 @@ module.exports = SectionHeaderTabs;
 
 
 var UIPrinciples = function UIPrinciples() {
-  return __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(
-    'div',
-    { className: 'wrap' },
-    __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(
-      'div',
-      { className: 'row' },
-      __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(
+    return __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(
         'div',
-        { className: 'columns small-12' },
+        { className: 'wrap' },
         __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(
-          'h1',
-          { className: 'heading' },
-          'Design Principles'
+            'div',
+            { className: 'row' },
+            __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(
+                'div',
+                { className: 'columns small-12' },
+                __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(
+                    'h1',
+                    { className: 'heading' },
+                    'Design Principles'
+                )
+            ),
+            __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(
+                'div',
+                { className: 'columns small-12' },
+                __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(
+                    'h2',
+                    null,
+                    'In Our Products'
+                ),
+                __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(
+                    'div',
+                    { className: 'principle-row principle-row-clarity' },
+                    __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(
+                        'h3',
+                        { className: 'principle-row-heading' },
+                        'Clarity'
+                    ),
+                    __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(
+                        'p',
+                        { className: 'principle-row-description' },
+                        'Through the use of familiar patterns, guided workflows, inline instructions and contextual information, our products are understood without the need for a manual, where possible, and provide easy access to concise and clear documentation where needed. Terminology is carefully considered and maps closely to the purpose or concept it describes. Cognitive load is offloaded onto the application wherever possible.'
+                    )
+                ),
+                __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(
+                    'div',
+                    { className: 'principle-row principle-row-integrity' },
+                    __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(
+                        'h3',
+                        null,
+                        'Integrity'
+                    ),
+                    __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(
+                        'p',
+                        null,
+                        'Information communicated to the user is accurate and thorough. Complete transparency instills confidence and trust in the resilience and safety of the product, which is crucial for managing mission critical infrastructure. Esoteric technical details are dialed back in primary views but are always accessible to users who seek them.'
+                    )
+                ),
+                __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(
+                    'div',
+                    { className: 'principle-row principle-row-inclusiveness' },
+                    __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(
+                        'h3',
+                        null,
+                        'Inclusiveness'
+                    ),
+                    __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(
+                        'p',
+                        null,
+                        'Our products serve everyone from beginners learning how to use MongoDB for the first time to experts in the field. They empathize with the user, anticipate their goals and needs to deliver an experience that feels custom tailored. When a beginner feels stuck, the product provides guidance. When an expert desires a shortcut or advanced feature, the product supplies it exactly as the user would expect.'
+                    )
+                ),
+                __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(
+                    'div',
+                    { className: 'principle-row principle-row-consistency' },
+                    __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(
+                        'h3',
+                        null,
+                        'Consistency'
+                    ),
+                    __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(
+                        'p',
+                        null,
+                        'We offer a full suite of products that fit into each stage of a user\u2019s lifecycle and workflow. Information and interaction patterns that appear in different places are consistent visually and conceptually. This allows users to move fluidly through our products, confident that what they learn in one will carry over to all others.'
+                    )
+                ),
+                __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(
+                    'div',
+                    { className: 'principle-row principle-row-automation' },
+                    __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(
+                        'h3',
+                        null,
+                        'Automation'
+                    ),
+                    __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(
+                        'p',
+                        null,
+                        'Our products convey an understanding that sometimes the best design is invisible to the user. While our products break complicated tasks into simple and intuitive flows, the biggest impact is achieved when the work is done for the user. Not just a matter of convenience, when we automate a task we reduce chance of user error, leading to a safer overall experience. Our products automate where possible while leaving control with the user where needed.'
+                    )
+                ),
+                __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(
+                    'h2',
+                    null,
+                    'How We Work'
+                ),
+                __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(
+                    'div',
+                    { className: 'principle-row principle-row-collaboration' },
+                    __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(
+                        'h3',
+                        null,
+                        'Collaboration'
+                    ),
+                    __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(
+                        'p',
+                        null,
+                        'We recognize that the best work comes from diligent collaboration. We work side-by-side with other designers, engineers, and product managers.'
+                    )
+                ),
+                __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(
+                    'div',
+                    { className: 'principle-row principle-row-rigor' },
+                    __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(
+                        'h3',
+                        null,
+                        'Rigor'
+                    ),
+                    __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(
+                        'p',
+                        null,
+                        'We aren\u2019t satisfied with our solutions until we\u2019ve tested them and proven their effectiveness. We validate ideas and concepts early in the design process, before any wireframing takes place. We aren\u2019t afraid to iterate quickly, leaving behind solutions we initially thought promising. Data is what guides us.'
+                    )
+                ),
+                __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(
+                    'div',
+                    { className: 'principle-row principle-row-passion' },
+                    __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(
+                        'h3',
+                        null,
+                        'Passion'
+                    ),
+                    __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(
+                        'p',
+                        null,
+                        'We are intensely passionate about our work. We strive for a level of excellence that delights the user. Even seemingly imperceptible details receive special attention. We do this so that our products go beyond mere utility to be cherished by the user.'
+                    )
+                ),
+                __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(
+                    'div',
+                    { className: 'principle-row principle-row-impact' },
+                    __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(
+                        'h3',
+                        null,
+                        'Impact'
+                    ),
+                    __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(
+                        'p',
+                        null,
+                        'We value impact over deliverables. We recognize that our tools and process, while constantly evolving and improving, are only a means to achieving positive outcomes for the user.'
+                    )
+                )
+            )
         )
-      ),
-      __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(
-        'div',
-        { className: 'columns small-12' },
-        __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(
-          'h2',
-          null,
-          'In Our Products'
-        ),
-        __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(
-          'h3',
-          null,
-          'Clarity'
-        ),
-        __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(
-          'p',
-          null,
-          'Through the use of familiar patterns, guided workflows, inline instructions and contextual information, our products are understood without the need for a manual, where possible, and provide easy access to concise and clear documentation where needed. Terminology is carefully considered and maps closely to the purpose or concept it describes. Cognitive load is offloaded onto the application wherever possible.'
-        ),
-        __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(
-          'h3',
-          null,
-          'Integrity'
-        ),
-        __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(
-          'p',
-          null,
-          'Information communicated to the user is accurate and thorough. Complete transparency instills confidence and trust in the resilience and safety of the product, which is crucial for managing mission critical infrastructure. Esoteric technical details are dialed back in primary views but are always accessible to users who seek them.'
-        ),
-        __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(
-          'h3',
-          null,
-          'Inclusiveness'
-        ),
-        __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(
-          'p',
-          null,
-          'Our products serve everyone from beginners learning how to use MongoDB for the first time to experts in the field. They empathize with the user, anticipate their goals and needs to deliver an experience that feels custom tailored. When a beginner feels stuck, the product provides guidance. When an expert desires a shortcut or advanced feature, the product supplies it exactly as the user would expect.'
-        ),
-        __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(
-          'h3',
-          null,
-          'Consistency'
-        ),
-        __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(
-          'p',
-          null,
-          'We offer a full suite of products that fit into each stage of a user\u2019s lifecycle and workflow. Information and interaction patterns that appear in different places are consistent visually and conceptually. This allows users to move fluidly through our products, confident that what they learn in one will carry over to all others.'
-        ),
-        __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(
-          'h3',
-          null,
-          'Automation'
-        ),
-        __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(
-          'p',
-          null,
-          'Our products convey an understanding that sometimes the best design is invisible to the user. While our products break complicated tasks into simple and intuitive flows, the biggest impact is achieved when the work is done for the user. Not just a matter of convenience, when we automate a task we reduce chance of user error, leading to a safer overall experience. Our products automate where possible while leaving control with the user where needed.'
-        ),
-        __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(
-          'h2',
-          null,
-          'How We Work'
-        ),
-        __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(
-          'h3',
-          null,
-          'Collaboration'
-        ),
-        __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(
-          'p',
-          null,
-          'We recognize that the best work comes from diligent collaboration. We work side-by-side with other designers, engineers, and product managers.'
-        ),
-        __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(
-          'h3',
-          null,
-          'Validation'
-        ),
-        __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(
-          'p',
-          null,
-          'We aren\u2019t satisfied with our solutions until we\u2019ve tested them and proven their effectiveness. We validate ideas and concepts early in the design process, before any wireframing takes place. We aren\u2019t afraid to iterate quickly, leaving behind solutions we initially thought promising. Data is what guides us.'
-        ),
-        __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(
-          'h3',
-          null,
-          'Spirit'
-        ),
-        __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(
-          'p',
-          null,
-          'We are intensely passionate about our work. We strive for a level of excellence that delights the user. Even seemingly imperceptible details receive special attention. We do this so that our products go beyond mere utility to be cherished by the user.'
-        ),
-        __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(
-          'h3',
-          null,
-          'Impact'
-        ),
-        __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(
-          'p',
-          null,
-          'We value impact over deliverables. We recognize that our tools and process, while constantly evolving and improving, are only a means to achieving positive outcomes for the user.'
-        )
-      )
-    )
-  );
+    );
 };
 
 /* harmony default export */ __webpack_exports__["a"] = (UIPrinciples);
